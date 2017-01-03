e28edb3ef198b08241f271f31809c2aa4625352d
rickapod <buddgarcia@gmailo.com>
2017-01-02 20:22:06 -0800

    starts the developement of the search bar

---
+++ client/app/components/Search_bar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class Search extends REact.component {
+  constructor(props) {
+    super(props);\;
+    this.state = {
+      SearchIndex: "Fashion";
+      Keywords: '';
+      ResponseGroup: 'Images, ImagesAttributes';
+      Sort: '';
+    }
+  }
+}
