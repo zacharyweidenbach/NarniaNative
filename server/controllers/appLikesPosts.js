a2833055b6e116b4a05d554be04de98ed7e21905
MrJonWu <mrjonwu@gmail.com>
2017-01-07 12:25:12 -0800

    work so far

--- server/controllers/appLikesPosts.js
+++ server/controllers/appLikesPosts.js
@@ -23,12 +23,12 @@ module.exports = {
       res.json(response);
     });
   },
-  // findLikedByUser: function(req, res, next) {
-  //   connection.query('SELECT * FROM likesPosts INNER JOIN posts on posts.Id = and posts.type="image" and likesPosts.userId=' + req.body.userId, function(err, result) {
-  //     var response = err || result;
-  //     res.json(response);
-  //   });
-  // }
+  findLikedPostId: function(req, res, next) {
+    connection.query('SELECT * FROM likesPosts where userId=' + req.body.userId, function(err, result) {
+      var response = err || result;
+      res.json(response);
+    });
+  }
 };
 
 // 'SELECT users.username, users.thumbnail, posts.id, posts.body, posts.description, posts.likesCount, posts.type, posts.createdAt FROM users INNER JOIN posts on users.id=posts.userId and posts.type="image"'
