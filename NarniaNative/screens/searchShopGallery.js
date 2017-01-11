8b5e4d8e2357a93ab9dd670066086206121ce4c4
rickapod <buddgarcia@gmailo.com>
2017-01-10 19:07:10 -0800

    adjust amazonAPI call to return object of information and adds dynamic modal to the shop search

--- NarniaNative/screens/searchShopGallery.js
+++ NarniaNative/screens/searchShopGallery.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
   StyleSheet,
-  Component,
   Dimensions,
   Image,
+  TouchableHighlight
 } from 'react-native';
+import ClothingModal from './clothingModal.js';
+
 
 const styles = StyleSheet.create({
   textStyle: {
@@ -34,19 +36,45 @@ const styles = StyleSheet.create({
   },
 });
 
-var SearchShopGallery = (props) => {
-  var clothing = props.items.map(function(item){
-    return (
-      <View style={styles.userContainer}>
-        <Image style={styles.imgSmall} source={{uri: item}} />   
-      </View>
-    )
-  });
-  return (
-    <View style={styles.container}>
-      {clothing}
-    </View>
-  );
-};
+export default class SearchShopGallery extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      clothingInd: 0,
+      modalVisible: false,
+    }
+  }
 
-module.exports = SearchShopGallery;
+  onButtonPress(button, index) {
+    switch (button) {
+      case 'toggleModal':
+        this.setState({clothingInd: index})
+        //alert(this.props.items[this.state.clothingInd])
+        this.setState({modalVisible: true});
+        break;
+    }
+  }
+  
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render () {
+    var clothing = this.props.items.map(function(item, index){
+      return (
+        <View key={index}>
+          <TouchableHighlight onPress={this.onButtonPress.bind(this, 'toggleModal', index)} underlayColor='transparent' >
+            <Image style={styles.imgSmall} source={{uri: item.image}} resizeMode={Image.resizeMode.contain}/>   
+          </TouchableHighlight>
+        </View>
+      )
+    }.bind(this));
+    return (
+      <View style={styles.container}>
+        {clothing}
+        {this.state.modalVisible ? <ClothingModal clothing={this.props.items[this.state.clothingInd]} modalVisible={this.state.modalVisible} setModalVisible={this.setModalVisible.bind(this)}/> : null}
+      </View>
+    );
+  };
+} 
+
