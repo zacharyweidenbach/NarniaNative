64966dabf701c34e391170cad85bd659f98fbade
Zachary Weidenbach <zweidenbach@gmail.com>
2017-01-11 17:18:48 -0800

    refactored loading page out to its own screen

---
+++ NarniaNative/screens/loading.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { TabViewAnimated, TabBarTop } from 'react-native-tab-view';
+import {
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+import Main from '../index.ios.js';
+import Signup from './signup.js';
+import ip from '../network.js';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    elevation: 2,
+    backgroundColor: '#fff'
+  }
+});
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.isLoggedIn(this);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={{fontWeight: 'bold', fontSize: 26}}>NARNIA</Text>
+        </View>
+      </View>
+    );
+  }
+}
