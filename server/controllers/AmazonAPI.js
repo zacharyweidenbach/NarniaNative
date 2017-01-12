c60c422912a1d8df18ed994feef8f67689d8d3b6
rickapod <buddgarcia@gmailo.com>
2017-01-11 18:52:45 -0800

    fixes amazonAPI request bug

--- server/controllers/AmazonAPI.js
+++ server/controllers/AmazonAPI.js
@@ -22,7 +22,7 @@ module.exports = {
       'ResponseGroup': 'Images,ItemAttributes',
       'ItemPage': req.body.page
     }).then((response) => {
-        //console.log("Results object: ", response.result.ItemSearchResponse.Items.Item[0]);
+        // console.log("Results object: ", response.result.ItemSearchResponse.Items.Item[0]);
         // console.log("Raw response body: ", response.responseBody);
     /*//creates MYsql insertion query syntax for dummy data
         var clothingSchema = "INSERT INTO `clothing` (detailUrl, smallImg, mediumImg, largeImg, brand, color, department, listPrice, productGroup, productTypeName, Title, UPC, ASIN) VALUES (" + '"' + response.result.ItemSearchResponse.Items.Item[0].DetailPageURL + '", "' + response.result.ItemSearchResponse.Items.Item[0].SmallImage.URL + '", "' + response.result.ItemSearchResponse.Items.Item[0].MediumImage.URL + '", "' + response.result.ItemSearchResponse.Items.Item[0].LargeImage.URL + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.Brand + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.Color + '", "' +response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.Department + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.ListPrice.FormattedPrice + '", "'+ response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.ProductGroup + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.ProductTypeName + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.Title + '", "' + response.result.ItemSearchResponse.Items.Item[0].ItemAttributes.UPC + '", "' + response.result.ItemSearchResponse.Items.Item[0].ASIN + '");'
@@ -31,8 +31,8 @@ module.exports = {
       AmazonResponse = response.result.ItemSearchResponse.Items.Item;
       var clothing = [];
       for (var i = 0; i < AmazonResponse.length; i++) {
-        if (AmazonResponse[i].LargeImage) {
-          clothing.push({Image:AmazonResponse[i].LargeImage.URL, DetailPageURL:AmazonResponse[i].DetailPageURL, ProductTypeName:AmazonResponse[i].ItemAttributes.ProductTypeName, Title:AmazonResponse[i].ItemAttributes.Title, ASIN:AmazonResponse[i].ASIN, UPC:AmazonResponse[i].ItemAttributes.UPC, Brand:AmazonResponse[i].ItemAttributes.Brand, Thumbnail:AmazonResponse[i].ImageSets.ImageSet.ThumbnailImage.URL, Color:AmazonResponse[i].ItemAttributes.Color});
+        if (AmazonResponse[i].LargeImage && AmazonResponse[i].ImageSets && AmazonResponse[i].ImageSets.ImageSet[0] && AmazonResponse[i].ImageSets.ImageSet[0].ThumbnailImage ) {
+          clothing.push({Image:AmazonResponse[i].LargeImage.URL, DetailPageURL:AmazonResponse[i].DetailPageURL, ProductTypeName:AmazonResponse[i].ItemAttributes.ProductTypeName, Title:AmazonResponse[i].ItemAttributes.Title, ASIN:AmazonResponse[i].ASIN, UPC:AmazonResponse[i].ItemAttributes.UPC, Brand:AmazonResponse[i].ItemAttributes.Brand, Thumbnail:AmazonResponse[i].ImageSets.ImageSet[0].ThumbnailImage.URL, Color:AmazonResponse[i].ItemAttributes.Color});
         }
       }
       console.log(clothing);
