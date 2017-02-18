25bc9c6379045346379a265a36079746bd011811
Zachary Weidenbach <zweidenbach@gmail.com>
2017-02-17 20:03:52 -0700

    refactored POSTfetch to include jwt in header

--- NarniaNative/utils.js
+++ NarniaNative/utils.js
@@ -1,4 +1,5 @@
 import ip from './network.js';
+import auth from './auth.js';
 
 export async function POSTfetch(path, body) {
   var api = 'http://' + ip.address + ':3000/api/' + path;
@@ -20,8 +21,8 @@ export async function POSTfetch(path, body) {
       }
     };
   }
-
   try {
+    info.headers['x-access-token'] = await auth.getToken();
     return fetch(api, info).then((res) => res.json());
   } catch(err) {
     console.log(err);
@@ -45,3 +46,29 @@ export async function GETfetch(path) {
   }
 };
 
+export async function POSTauth(path, body) {
+  var api = 'http://' + ip.address + ':3000/api/' + path;
+  if (body) {
+    var info = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    };
+  } else {
+    var info = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    };
+  }
+  try {
+    return fetch(api, info).then((res) => res.json());
+  } catch(err) {
+    console.log(err);
+  }
+};
